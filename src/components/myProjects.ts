2e7c927af1666ed2aace56caaa8a61beb6254a22
Brandon Pinzon <brandonpinzont@gmail.com>
2025-05-19 13:07:39 -0500

    Added link to muebles casa river demo

--- src/components/myProjects.ts
+++ src/components/myProjects.ts
@@ -1,30 +1,68 @@
 import { IProject } from "@/Interfaces/IProject";
 
 export const myProjects: IProject[] = [
-    {
-       "title": "Muebles Casa River MVP (PRÓXIMAMENTE)",
-        "image": "./MueblesCasaRiverMosaic.png",
-        "technologies": [" HTML ", "-", " CSS ", "-", " Tailwind ", "-", " Figma ", "-", " JavaScript ", "-", " Next.js "],
-        "repo": "https://github.com/B9inzon/MueblesCasaRiver",
-        "description":"Este proyecto funciona como un catálogo online para una fábrica de muebles en el que se va a dirigir al cliente a cotizar directamente con los fabricantes por whatsapp. Por el momento no se va a integrar plataforma de pagos, pero se va a ofrecer toda la información pertinente para que el cliente pueda cotizar la fabricación de sus muebles directamente con los fabricantes.",
-        "date": "Abr - 2025", 
-    },
-    {
-        "title": "Game Vault",
-        "demo":"https://gamevault-frontend.vercel.app/",
-        "image": "./Game-Vault-landing.jpg",
-        "technologies": [" HTML ", "-", " CSS ", "-", " Tailwind ", "-", " Figma ", "-", " TypeScript ", "-", " Next.js "],
-        "repo": "https://github.com/efetine/gamevault-frontend/tree/main",
-        "description":"Este e-commerce tenía como objetivo la venta de videojuegos digitales, consolas, tarjetas de regalo y accesorios.",
-        "date": "Nov - 2024",
-    },
-    {
-        "title": "Think-Tech",
-        "image": "./Think-Tech-landing.jpg",
-        "technologies": [" Next.js ", "-", " Tailwind ", "-", " Figma "," CSS ", "-", " JavaScript ", "-", " PostegreSQL " ],
-        "repo": "https://github.com/B9inzon/Think-Tech",
-        "description":"Este e-commerce tenía como objetivo la venta de dispositivos de tecnología como computadores, celulares, accesorios entre otros.",
-        "date": "Oct - 2024",
-
-    },   
-]
+  {
+    title: "Muebles Casa River MVP (PRÓXIMAMENTE)",
+    demo: "mueblescasariver.vercel.app",
+    image: "./MueblesCasaRiverMosaic.png",
+    technologies: [
+      " HTML ",
+      "-",
+      " CSS ",
+      "-",
+      " Tailwind ",
+      "-",
+      " Figma ",
+      "-",
+      " JavaScript ",
+      "-",
+      " Next.js ",
+    ],
+    repo: "https://github.com/B9inzon/MueblesCasaRiver",
+    description:
+      "Este proyecto funciona como un catálogo online para una fábrica de muebles en el que se va a dirigir al cliente a cotizar directamente con los fabricantes por whatsapp. Por el momento no se va a integrar plataforma de pagos, pero se va a ofrecer toda la información pertinente para que el cliente pueda cotizar la fabricación de sus muebles directamente con los fabricantes.",
+    date: "Abr - 2025",
+  },
+  {
+    title: "Game Vault",
+    demo: "https://gamevault-frontend.vercel.app/",
+    image: "./Game-Vault-landing.jpg",
+    technologies: [
+      " HTML ",
+      "-",
+      " CSS ",
+      "-",
+      " Tailwind ",
+      "-",
+      " Figma ",
+      "-",
+      " TypeScript ",
+      "-",
+      " Next.js ",
+    ],
+    repo: "https://github.com/efetine/gamevault-frontend/tree/main",
+    description:
+      "Este e-commerce tenía como objetivo la venta de videojuegos digitales, consolas, tarjetas de regalo y accesorios.",
+    date: "Nov - 2024",
+  },
+  {
+    title: "Think-Tech",
+    image: "./Think-Tech-landing.jpg",
+    technologies: [
+      " Next.js ",
+      "-",
+      " Tailwind ",
+      "-",
+      " Figma ",
+      " CSS ",
+      "-",
+      " JavaScript ",
+      "-",
+      " PostegreSQL ",
+    ],
+    repo: "https://github.com/B9inzon/Think-Tech",
+    description:
+      "Este e-commerce tenía como objetivo la venta de dispositivos de tecnología como computadores, celulares, accesorios entre otros.",
+    date: "Oct - 2024",
+  },
+];
